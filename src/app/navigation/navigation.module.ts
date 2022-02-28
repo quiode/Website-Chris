741611cf7adb575297f6ab5515ae6811e51350c9
Dominik Schwaiger <domi.schwaiger04@gmail.com>
2022-02-28 10:44:36 +0100

    nav stiling

---
+++ src/app/navigation/navigation.module.ts
@@ -0,0 +1,10 @@
+import { NavigationComponent } from './navigation.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [NavigationComponent],
+  imports: [CommonModule],
+  exports: [NavigationComponent],
+})
+export class NavigationModule {}
