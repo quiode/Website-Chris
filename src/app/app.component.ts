434147582108888e4f8e6b712348785d9f2a6ad5
Dominik Schwaiger <51075975+quiode@users.noreply.github.com>
2022-03-01 18:55:26 +0100

    darkmode (+button)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, Input, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private cookieService: CookieService) {}
+
+  ngOnInit(): void {
+    if (this.getDarkMode()) {
+      this.setDarkMode(true);
+    } else {
+      this.setDarkMode(false);
+    }
+  }
+
   title = 'Website-Chris';
+  @HostBinding('style.--primary-color')
+  @Input()
+  primaryColor: string | undefined;
+  @HostBinding('style.--secondary-color')
+  @Input()
+  secondaryColor: string | undefined;
+
+  /**
+   * @description enables or disables the dark mode color scheme for the website
+   * @param darkMode true to enable dark mode, false to disable
+   */
+  setDarkMode(darkMode: boolean) {
+    if (darkMode) {
+      this.primaryColor = 'var(--dark-primary)';
+      this.secondaryColor = 'var(--dark-secondary)';
+    } else {
+      this.primaryColor = 'var(--white-primary)';
+      this.secondaryColor = 'var(--white-secondary)';
+    }
+    this.cookieService.set('darkMode', '' + darkMode);
+  }
+
+  getDarkMode(): boolean {
+    if (this.cookieService.check('darkMode')) {
+      return this.cookieService.get('darkMode') == 'true';
+    } else {
+      this.setDarkMode(false);
+      return false;
+    }
+  }
+
+  onclick() {
+    this.setDarkMode(!this.getDarkMode());
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CookieService } from 'ngx-cookie-service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,7 +9,7 @@ import { NavigationModule } from './navigation/navigation.module';
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, NavigationModule],
-  providers: [],
+  providers: [CookieService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
