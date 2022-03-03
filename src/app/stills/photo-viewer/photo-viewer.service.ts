a678266a66440dac45b9b3e193e5bae94927536e
Dominik Schwaiger <51075975+quiode@users.noreply.github.com>
2022-03-03 20:51:34 +0100

    photo viewer service

--- src/app/stills/photo-viewer/photo-viewer.service.ts
+++ src/app/stills/photo-viewer/photo-viewer.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PhotoViewerService {
+  constructor() {}
 
-  constructor() { }
+  nextImageUrl(currentId: string): string {}
+
+  prevImageUrl(currentId: string): string {}
 }
