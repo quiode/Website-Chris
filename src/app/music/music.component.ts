c1c9e176da40ec41a6abb90056ad09e08fb21406
Dominik Schwaiger <51075975+quiode@users.noreply.github.com>
2022-03-12 23:18:08 +0100

    favicons

--- src/app/music/music.component.ts
+++ src/app/music/music.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import videojs from 'video.js';
+import { Music, MusicService } from './music.service';
 
 @Component({
   selector: 'app-music',
   templateUrl: './music.component.html',
   styleUrls: ['./music.component.scss'],
 })
-export class MusicComponent {
-  constructor() {}
+export class MusicComponent implements OnInit {
+  music: Music[] = [];
+  constructor(private musicService: MusicService) {}
+
+  ngOnInit() {
+    this.musicService.music.subscribe((music) => {
+      this.music = music;
+    });
+  }
 }
