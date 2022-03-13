6123252569280d93411fd1c35a0fded5ee9562f7
Dominik Schwaiger <51075975+quiode@users.noreply.github.com>
2022-03-13 17:26:39 +0100

    chrome

--- src/app/music/music-item/music-item.component.ts
+++ src/app/music/music-item/music-item.component.ts
@@ -38,4 +38,23 @@ export class MusicItemComponent implements OnInit {
       window.open('http://' + this.music?.url, '_blank');
     }
   }
+
+  detectBrowserName() {
+    const agent = window.navigator.userAgent.toLowerCase()
+    switch (true) {
+      case agent.indexOf('edge') > -1:
+        return 'edge';
+      case agent.indexOf('opr') > -1 && !!(<any>window).opr:
+        return 'opera';
+      case agent.indexOf('chrome') > -1 && !!(<any>window).chrome:
+        return 'chrome';
+      case agent.indexOf('trident') > -1:
+        return 'ie';
+      case agent.indexOf('firefox') > -1:
+        return 'firefox';
+      case agent.indexOf('safari') > -1:
+        return 'safari';
+      default:
+        return 'other';
+    }
 }
