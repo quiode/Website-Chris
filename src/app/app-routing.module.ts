569dfdc187715f8da8e075d694f202b0a2b0393d
Dominik Schwaiger <mail@dominik-schwaiger.ch>
2023-06-16 14:09:03 +0200

    calculate age in about me based on birthdate

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,21 @@
-import { AdminVideosComponent } from './admin-panel/videos/videos.component';
-import { AdminStillsComponent } from './admin-panel/stills/stills.component';
-import { MusicComponent } from './music/music.component';
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { ImpressumComponent } from './impressum/impressum.component';
-import { LinktreeComponent } from './linktree/linktree.component';
-import { StillsComponent } from './stills/stills.component';
-import { AboutmeComponent } from './aboutme/aboutme.component';
-import { NotfoundComponent } from './notfound/notfound.component';
-import { VideosComponent } from './videos/videos.component';
-import { PhotoViewerComponent } from './stills/photo-viewer/photo-viewer.component';
-import { AdminPanelComponent } from './admin-panel/admin-panel.component';
-import { LoginComponent } from './admin-panel/login/login.component';
-import { AdminMusicComponent } from './admin-panel/music/music.component';
-import { AuthGuard } from './admin-panel/auth.guard';
-import { LoginGuard } from './admin-panel/login.guard';
-import { VideoPlayerComponent } from './shared/video-player/video-player.component';
+import {AdminVideosComponent} from './admin-panel/videos/videos.component';
+import {AdminStillsComponent} from './admin-panel/stills/stills.component';
+import {MusicComponent} from './music/music.component';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {ImpressumComponent} from './impressum/impressum.component';
+import {LinktreeComponent} from './linktree/linktree.component';
+import {StillsComponent} from './stills/stills.component';
+import {AboutMeComponent} from './aboutme/about-me.component';
+import {NotfoundComponent} from './notfound/notfound.component';
+import {VideosComponent} from './videos/videos.component';
+import {PhotoViewerComponent} from './stills/photo-viewer/photo-viewer.component';
+import {AdminPanelComponent} from './admin-panel/admin-panel.component';
+import {LoginComponent} from './admin-panel/login/login.component';
+import {AdminMusicComponent} from './admin-panel/music/music.component';
+import {AuthGuard} from './admin-panel/auth.guard';
+import {LoginGuard} from './admin-panel/login.guard';
+import {VideoPlayerComponent} from './shared/video-player/video-player.component';
 
 const routes: Routes = [
   {
@@ -57,7 +57,7 @@ const routes: Routes = [
     component: VideosComponent,
     children: [{ path: ':id', component: VideoPlayerComponent }],
   },
-  { path: 'aboutme', component: AboutmeComponent },
+  { path: 'aboutme', component: AboutMeComponent },
   { path: '', redirectTo: '/stills', pathMatch: 'full' },
   { path: '**', component: NotfoundComponent },
 ];
